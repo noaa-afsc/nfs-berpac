90419b19e5d436101a59ceea78f2a529f5c803bf
Josh M. London <josh.london@noaa.gov>
2023-11-28 10:48:41 -0800

    pull ids with wcGetDeployID

--- R/get_wc_data.R
+++ R/get_wc_data.R
@@ -1,16 +1,21 @@
-get_wc_data <- function() {
-  ids <- c("6333a5fc17e8480e28ae482a",
-           "6333a5fc17e8480e28ae482c",
-           "6333a5fc17e8480e28ae482e",
-           "6333a5fc17e8480e28ae4830",
-           "6333a5fc17e8480e28ae4832",
-           "6333c6eb17e8480e28ae4fe0",
-           "633601df17e8480e28ae9e99",
-           "633601e017e8480e28ae9e9d",
-           "633601e017e8480e28ae9e9f",
-           "633601e017e8480e28ae9ea4",
-           "633601e017e8480e28ae9eaa",
-           "633610f017e8480e28aea03e")
+get_wc_data <- function(deployids) {
+  
+  ids <- purrr::map_dfr(
+    deployids,~ wcUtils::wcGetDeployID(wcUtils::wcPOST(), 
+                                                 deployid = .x)
+  ) |> pull(ids)
+  # ids <- c("6333a5fc17e8480e28ae482a",
+  #          "6333a5fc17e8480e28ae482c",
+  #          "6333a5fc17e8480e28ae482e",
+  #          "6333a5fc17e8480e28ae4830",
+  #          "6333a5fc17e8480e28ae4832",
+  #          "6333c6eb17e8480e28ae4fe0",
+  #          "633601df17e8480e28ae9e99",
+  #          "633601e017e8480e28ae9e9d",
+  #          "633601e017e8480e28ae9e9f",
+  #          "633601e017e8480e28ae9ea4",
+  #          "633601e017e8480e28ae9eaa",
+  #          "633610f017e8480e28aea03e")
   
   res <- purrr::map(ids, wcGetDownload)
   
